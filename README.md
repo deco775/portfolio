8d8b4e6dc634089f73575343b5b93e798a75c533
deco775 <mkansipitso@gmail.com>
2025-03-16 14:58:16 +0200

    portfolio

---
+++ README.md
@@ -0,0 +1,17 @@
+# Portfolio Website
+
+Welcome to my personal portfolio website! This site showcases my projects, skills, and experiences. It is designed to highlight my work and provide an easy way for potential clients, employers, and collaborators to get in touch.
+
+## Features
+
+- **About Me** section  
+- **Projects** gallery with descriptions  
+- **Contact** form and social media links  
+- **Responsive Design** (mobile-friendly)
+
+## Technologies Used
+
+- HTML5  
+- CSS3  
+- JavaScript  
+
---
+++ index.js
@@ -0,0 +1,66 @@
+// script.js
+const buttons = document.querySelectorAll('.aSkills');
+const targets = document.querySelectorAll('.skillSet');
+
+
+let prevButton = null;
+let prevTarget = null;
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    if (prevButton) {
+      prevButton.style.backgroundColor = '';
+      prevTarget.style.color = '';
+      prevTarget.style.display = 'none';
+    }
+
+    button.style.backgroundColor = 'red';
+    targets[index].style.display = 'block';
+    
+    prevButton = button;
+    prevTarget = targets[index];
+
+  });
+});
+
+const downloadLink = document.getElementById('cvBtn');
+const cvFile = "CV/Pitso's_CV.pdf";
+downloadLink.href = cvFile;
+downloadLink.download = "Pitso's_CV.pdf";
+
+/*const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  fetch('https://formspree.io/f/xpwpwdpb', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+});*/
+
+const form = document.getElementById('contact-form');
+const messageInput = document.getElementById('msg'); // assuming this is the message input field
+const emailInput = document.getElementById('email'); // assuming this is the email input field
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  fetch('https://formspree.io/f/xpwpwdpb', {
+    method: 'POST',
+    body: formData,
+    mode: 'no-cors',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      alert('Sent successfully!'); // display success message
+      messageInput.value = ''; // clear message input field
+      emailInput.value = ''; // clear email input field
+    })
+    .catch((error) => console.error(error));
+});
+
